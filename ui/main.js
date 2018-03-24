c26e2b17a10087ea889c14aedef7d95d15f2ff48
deepshikha123gupta <34314904+deepshikha123gupta@users.noreply.github.com>
2018-03-24 19:57:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,12 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New Value';
+var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight(){
+    marginleft = marginleft + 1;
+    img.style.marginleft = marginleft + 'px';   
+}
+imag.Onclick = function () {
+    var interval = setInterval(moveRight, 50);
+};
