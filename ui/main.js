b414088879924bfd310b3366780ac863e2e2e2b3
deepshikha123gupta <34314904+deepshikha123gupta@users.noreply.github.com>
2018-03-28 17:47:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,6 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-element.innerHTML='New Value';
-var img=document.getElementById('madi');
-var marginleft=0;
-function moveRight(){
-    marginleft = marginleft + 10;
-    img.style.marginleft = marginleft + 'px';   
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight, 50);
-};
+var button = document.getElementById('counter');
+var counter=0;
+
 button.onclick = function(){
     counter = counter + 1;
     var span = document.getElementById('count');
