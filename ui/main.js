b4613315d18a352a8bfbaab92442bad1db87b267
deepshikha123gupta <34314904+deepshikha123gupta@users.noreply.github.com>
2018-03-28 18:15:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,6 +14,6 @@ button.onclick = function(){
         }
         
     };
-    request.open('GET','http://deepjuhi123.imad.hasura-app.io/counter',true);
-    
+    request.open('POST','http://deepjuhi123.imad.hasura-app.io/counter',true);
+    request.send(null);  
 };
